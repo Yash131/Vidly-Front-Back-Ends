cd545d7cccc92c3b2867967a602c3195f6242209
unknown <yashchouriya131@gmail.com>
2021-03-13 19:22:24 +0530

    upcoming movie control added

--- BackEnd/startup/db.js
+++ BackEnd/startup/db.js
@@ -4,7 +4,6 @@ const config = require("config");
 
 module.exports = function () {
   let db = config.get('db')
-  db = "mongodb+srv://admin:admin1234@vidly.hsmmg.mongodb.net/vidlystore?authSource=admin&replicaSet=atlas-dxivrc-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
   mongoose
     .connect(db, {
       useNewUrlParser: true,
